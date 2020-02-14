b5fb02873d389cd4e0d2bd6679923c9cf45afd42
Jethet <henriette.hettinga@gmail.com>
2020-02-14 09:05:18 +0100

    Add router for login plus hbs page

---
+++ routes/login.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var loginRouter = express.Router();
+// const User = require('./../models/User');
+// const bcrypt = require('bcrypt')
+
+/* GET home page. */
+loginRouter.get('/login', function(req, res, next) {
+    res.render('auth-views/login');
+  });
+ 
+  
+
+  module.exports = loginRouter;
