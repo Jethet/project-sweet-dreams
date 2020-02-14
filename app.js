eaae07da269346a3d8b7c736eec8d0ff33bc0b00
Jethet <henriette.hettinga@gmail.com>
2020-02-14 12:03:16 +0100

    Update README.md

--- app.js
+++ app.js
@@ -2,13 +2,28 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const hbs          = require('hbs');
+const mongoose     = require('mongoose');
+var logger        =  require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 
 var app = express();
+
+const session    = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+
+mongoose
+  .connect('mongodb://localhost/sweet-dreamsDB', {useNewUrlParser: true})
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,6 +34,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+  session({
+    secret: "basic-auth-secret",
+    // cookie: { maxAge: 3600000 * 1 },	// 1 hour
+    resave: true,
+    saveUninitialized: false,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 60 * 60 * 24 * 7 // Time to live - 7 days (14 days - Default)
+    })
+  })
+);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var loginRouter = express.Router();
-// const User = require('./../models/User');
+const User = require('./../models/User');
 // const bcrypt = require('bcrypt')
 
 /* GET home page. */
-loginRouter.get('/login', function(req, res, next) {
+loginRouter.get('/', function(req, res, next) {
     res.render('auth-views/login');
   });
  
