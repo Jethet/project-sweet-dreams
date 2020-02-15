e4380d37604d475c80d9e3955f598a830a533b32
Jethet <henriette.hettinga@gmail.com>
2020-02-15 11:05:06 +0100

    Add user model

--- routes/login.js
+++ routes/login.js
@@ -8,6 +8,40 @@ loginRouter.get('/', function(req, res, next) {
     res.render('auth-views/login');
   });
  
-  
 
-  module.exports = loginRouter;
+loginRouter.post('/', (req, res, next) => {
+    const { username, password} = req.body;
+    console.log("Print req.body", req.body)
+    if(username === "" || password === ""){
+        res.render("auth-views/login", {
+            errorMessage: "Please provide the required username and password"
+        });
+        return;
+    };
+    User.findOne({username})
+    .then( (user) => {
+        if (!user) {
+            res.render("auth-views/login", {
+                errorMessage: "Username does not exist"
+        });
+        return;
+    }
+
+    const passwordFromDB = user.password;
+    const passwordCorrect = bcrypt.compareSync(password, passwordFromDB);
+
+    if (passwordCorrect) {
+        req.session.currentUser = user;
+        console.log(user)
+        res.redirect('/')
+        } else {
+        res.render("auth-views/login", {
+            errorMessage: "Incorrect password"
+        });
+        }
+    }) .catch(err => console.log(err));
+});
+
+
+
+module.exports = loginRouter;
