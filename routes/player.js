6717f211a851fd302180c9489f2d956d0f71a6dd
annkay108 <annkhachhibhoya@gmail.com>
2020-02-17 17:39:08 +0100

    updated

--- routes/player.js
+++ routes/player.js
@@ -1,7 +1,7 @@
 var express = require('express');
 const siteRouter = require('./home');
 // var playerRouter = express.Router();
-const User = require('../models/User');
+// const User = require('./../models/User');
 
 
 
