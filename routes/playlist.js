eac3687668eda25b4918a57a7d7f54493e671643
annkay108 <annkhachhibhoya@gmail.com>
2020-02-17 17:45:29 +0100

    updated the ports

--- routes/playlist.js
+++ routes/playlist.js
@@ -2,7 +2,7 @@ var express = require('express');
 const siteRouter = require('./home');
 // var playlistRouter = express.Router();
 // var playerRouter = express.Router();
-const User = require('../models/User');
+const User = require('./../models/User');
 
 
 //module.exports = playlistRouter;
