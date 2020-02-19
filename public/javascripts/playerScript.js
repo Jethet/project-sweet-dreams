6d4751b4648fb711b78cc2c27159730401972947
annkay108 <annkhachhibhoya@gmail.com>
2020-02-19 16:11:46 +0100

    player working

--- public/javascripts/playerScript.js
+++ public/javascripts/playerScript.js
@@ -1,24 +1,31 @@
 var playlistId = document.getElementById("playlist-id");
+var addAudio = document.getElementById("addAudio");
 
-console.log(playlistId.value);
-var playlist =["http://listen.vo.llnwd.net/g3/4/5/4/1/3/1282131454.mp3",
-    "http://listen.vo.llnwd.net/g3/2/6/8/1/6/1282561862.mp3",
-    "http://listen.vo.llnwd.net/g3/6/2/2/4/8/1396984226.mp3",
-    "http://listen.vo.llnwd.net/g2/7/9/1/4/6/896564197.mp3"];
+var playlist = [];
 
 if(playlistId.value.charAt(0)=="p"){
+    axios.get(`https://api.napster.com/v2.0/playlists/${playlistId.value}/tracks?apikey=ZTk2YjY4MjMtMDAzYy00MTg4LWE2MjYtZDIzNjJmMmM0YTdm&limit=200`)
+    .then(response => {
+        let arr = response.data.tracks;
+        arr.forEach(element => {
+            playlist.push(element.previewURL);
+        });
+    }).catch((err) => {
+        console.log(err)
+    });
     function play(){
         var audioElement = document.createElement('audio');
-        console.log(audioElement);
         audioElement.addEventListener("ended", playAudio);
         var i = 0;
         playAudio();
-    function playAudio() {
-        var entry = playlist[i++];       // get current entry, increment i
-        if (i >= playlist.length) i = 0; // if i=> length, reset
-        audioElement.src = entry;  
-        audioElement.load();             // cleanup old fun, invoke loading of new
-        audioElement.play();             // cue up play
-    }   
+        function playAudio() {
+            var entry = playlist[i++];       
+            if (i >= playlist.length) i = 0; 
+            audioElement.setAttribute("controls","");
+            audioElement.src = entry;
+            addAudio.appendChild(audioElement);
+            audioElement.load();             
+            audioElement.play();           
+        }  
     }
 }
