ad308eb31378a1423e935add0efc840a5867e62e
annkay108 <annkhachhibhoya@gmail.com>
2020-02-19 14:17:52 +0100

    updated the player

--- public/javascripts/playerScript.js
+++ public/javascripts/playerScript.js
@@ -1,7 +1,24 @@
 var playlistId = document.getElementById("playlist-id");
-console.log(typeof playlistId.value);
-function play(){
-    if(playlistId.value.charAt(0)=="p"){
-        console.log("something");
-    }
+
+console.log(playlistId.value);
+var playlist =["http://listen.vo.llnwd.net/g3/4/5/4/1/3/1282131454.mp3",
+    "http://listen.vo.llnwd.net/g3/2/6/8/1/6/1282561862.mp3",
+    "http://listen.vo.llnwd.net/g3/6/2/2/4/8/1396984226.mp3",
+    "http://listen.vo.llnwd.net/g2/7/9/1/4/6/896564197.mp3"];
+
+if(playlistId.value.charAt(0)=="p"){
+    function play(){
+        var audioElement = document.createElement('audio');
+        console.log(audioElement);
+        audioElement.addEventListener("ended", playAudio);
+        var i = 0;
+        playAudio();
+    function playAudio() {
+        var entry = playlist[i++];       // get current entry, increment i
+        if (i >= playlist.length) i = 0; // if i=> length, reset
+        audioElement.src = entry;  // <- for demo samples only. 'sound/' + entry.value + '.wav';
+        audioElement.load();             // cleanup old fun, invoke loading of new
+        audioElement.play();             // cue up play
+    }   
+}
 }
--- routes/player.js
+++ routes/player.js
@@ -4,46 +4,48 @@ const axios = require('axios');
 const User = require("./../models/User")
 var playerRouter = express.Router();
 
-// playerRouter.get('/:id',(req, res,next)=>{
-//     axios.get(`https://api.napster.com/v2.0/playlists/${req.params.id}/tracks?apikey=ZTk2YjY4MjMtMDAzYy00MTg4LWE2MjYtZDIzNjJmMmM0YTdm&limit=200`)
-//     .then(apiResponse => {
-//         const track = apiResponse.data.tracks;
-//         return track;
-//     })
-//     .then( track => {
-//         User.findById(req.session.currentUser._id)
-//         .populate('playlists')
-//         .then( userInfo => {
-//             let userHasPlaylists = false;
-//             if(userInfo.playlists.length !== 0) userHasPlaylists = true;
-
-//             const data = {
-//                 track: track,
-//                 userInfo: userInfo,
-//                 userHasPlaylists: userHasPlaylists
-//             }
-//             //console.log(data.track);
-//             console.log("userhas playlist", userHasPlaylists);
-            
-//             res.render("player",data);
-//         })
-//         .catch(err => console.log(err))
-//     })
-//     .catch(err=>console.log(err))
-// })
-
 playerRouter.get('/:id',(req, res,next)=>{
-    User.findById(req.session.currentUser._id)
+    axios.get(`https://api.napster.com/v2.0/playlists/${req.params.id}/tracks?apikey=ZTk2YjY4MjMtMDAzYy00MTg4LWE2MjYtZDIzNjJmMmM0YTdm&limit=200`)
+    .then(apiResponse => {
+        const track = apiResponse.data.tracks;
+        console.log(track);
+        return track;
+    })
+    .then( track => {
+        User.findById(req.session.currentUser._id)
         .populate('playlists')
         .then( userInfo => {
             let userHasPlaylists = false;
             if(userInfo.playlists.length !== 0) userHasPlaylists = true;
+
             const data = {
                 id: req.params.id,
+                track: track,
                 userInfo: userInfo,
                 userHasPlaylists: userHasPlaylists
             }
+            //console.log(data.track);
+            console.log("userhas playlist", userHasPlaylists);
+            
             res.render("player2",data);
         })
+        .catch(err => console.log(err))
+    })
+    .catch(err=>console.log(err))
 })
+
+// playerRouter.get('/:id',(req, res,next)=>{
+//     User.findById(req.session.currentUser._id)
+//         .populate('playlists')
+//         .then( userInfo => {
+//             let userHasPlaylists = false;
+//             if(userInfo.playlists.length !== 0) userHasPlaylists = true;
+//             const data = {
+//                 id: req.params.id,
+//                 userInfo: userInfo,
+//                 userHasPlaylists: userHasPlaylists
+//             }
+//             res.render("player2",data);
+//         })
+// })
 module.exports = playerRouter;
